0aacf2cf9b9cea9353abecb420450dad99549d87
Prateek <Prateek.Chauhan@energyaustralia.com.au>
2020-07-21 18:37:57 +1000

    pojo class for postcode lookup call

---
+++ src/main/java/pojo/postcodeLookup.java
@@ -0,0 +1,22 @@
+package pojo;
+
+public class postcodeLookup {
+    private int postcode;
+    private String customerType;
+
+    public int getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(int postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(String customerType) {
+        this.customerType = customerType;
+    }
+}
